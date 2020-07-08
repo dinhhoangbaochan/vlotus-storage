75c4d4e5f0efbbb3246e06b3b8eef7e41eb948e6
dinhhoangbaochan <dinhhoangbaochan@gmail.com>
2020-07-08 16:28:24 +0700

    Update

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,7 +1,82 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+
+<div class="account_form">
+    <div class="container">
+        <div class="row form_row justify-content-center">
+            <div class="col-6">
+                <img src="{{ asset('img/banner_storage.jpg') }}" alt="">
+            </div>
+            <div class="col-6 d-flex flex-column justify-content-center">
+                <form method="POST" action="{{ route('login') }}">
+                    @csrf
+
+                    <div class="form-group row">
+
+                        <div class="col-md-10">
+                            <input id="email" type="email" placeholder="Địa chỉ Email" class="form_ip form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+
+                            @error('email')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
+                        </div>
+                    </div>
+
+                    <div class="form-group row">
+                        
+
+                        <div class="col-md-10">
+                            <input id="password" placeholder="Mật khẩu" type="password" class="form_ip form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+
+                            @error('password')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
+                        </div>
+                    </div>
+
+                    <div class="form-group row">
+                        <div class="col-md-6">
+                            <div class="form-check">
+                                <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+
+                                <label class="form-check-label" for="remember">
+                                    {{ __('Remember Me') }}
+                                </label>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="form-group row mb-0">
+                        <div class="col-md-8">
+                            <button type="submit" class="btn btn-primary">
+                                {{ __('Đăng Nhập') }}
+                            </button>
+
+                            @if (Route::has('password.request'))
+                                <a class="btn btn-link btn-action" href="{{ route('password.request') }}">
+                                    {{ __('Quên Mật Khẩu?') }}
+                                </a>
+                            @endif
+                        </div>
+                    </div>
+
+                    <hr>
+
+                    <div class="form-group mt-3">
+                        <span>Nếu bạn không có tài khoản, hãy nhấn vào đây <a href="/request-account">Thiết lập tài khoản</a></span> 
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+
+{{-- <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
@@ -73,5 +148,5 @@
             </div>
         </div>
     </div>
-</div>
+</div> --}}
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', 'PagesController@index');
+Route::get('/', function () {
+	return redirect('/login');
+});
 Route::get('report', function () { return "Reports"; });
 
 
