4a1e07fe231281587b1862077eee933862fc3eb0
dinhhoangbaochan <dinhhoangbaochan@gmail.com>
2020-07-18 09:35:38 +0700

    Update

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,13 +1,74 @@
 $(document).ready(function() {
 
 
-  	let simpleArray = ["Hello", "Xin Chao", "Annhon", "Bonjour"];
+        var productsOrdered = [];
 
-    $('.autocomplete').autocomplete({
-        source: simpleArray,
-    }).focus(function () {
-        $(this).data("uiAutocomplete").search($(this).val());
-    });
+        console.log(productsOrdered.length);
+
+        $("#look_for_product").keyup(function(event) {
+            var getInput = $(this).val();
+
+            $.ajax({
+                url: "{{ url('search-product') }}",
+                method: "GET",
+                data: { input: getInput, },
+                success: function(res) {
+                    $("#findProductList").addClass("show");
+                    $("#findProductList").html(res);
+                },
+                error: function(err) {
+                    console.log(err);
+                }
+            });
+
+        });
+
+        $(document).on("click", ".dropdown-item" , function(event){
+            event.preventDefault();
+            var currentDataID = $(this).data("id");
+            console.log(currentDataID);
+
+            $.ajax({
+                url: "{{ url('get-selected-product') }}",
+                method: "GET",
+                dataType: "json",
+                data: { currentID : currentDataID },
+                success: function(res) {
+                    productsOrdered.push(res);
+                    console.log(productsOrdered);
+                    var valueArr = productsOrdered.map(function(item){ return item.id });
+                    var isDuplicate = valueArr.some(function(item, index){ 
+                        return valueArr.indexOf(item) != index 
+                    });
+
+                    if ( isDuplicate == true ) {
+                        productsOrdered.pop();
+                        console.log(productsOrdered);
+
+                    } else {
+                        console.log('everything is fine');
+
+                        var i;
+                        for ( i = 0; i < productsOrdered.length; i++ ) {
+                            console.log("heeee");
+                        }
+
+
+                    }
+
+                },
+                error: function(err) {
+                    console.log(err);
+                }
+            });
+
+
+        });
+
+
+
+        
+
 
 
 })
