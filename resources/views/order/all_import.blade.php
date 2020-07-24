abea62e7a3060f16c2dbf15d75231e2cf8d9b69b
dinhhoangbaochan <dinhhoangbaochan@gmail.com>
2020-07-24 13:23:59 +0700

    Update all_import.blade.php

--- resources/views/order/all_import.blade.php
+++ resources/views/order/all_import.blade.php
@@ -22,18 +22,50 @@
                     <a href="/orders/create-import" class="btn btn-outline-dark">Tạo đơn nhập +</a>
                 </div>
 
-                <ul>
+                <table class="lotus_table">
+                    
+                    <thead>
+                        <tr>
+                            <th rowspan="1" colspan="1">Mã đơn</th>
+                            <th rowspan="1" colspan="1">Nơi lưu trữ</th>
+                            <th rowspan="1" colspan="1">Ngày tạo đơn</th>
+                            <th>Ngày hẹn giao</th>
+                            <th>Trạng thái</th>
+                        </tr>
+                    </thead>
 
-                @if( count( $importOrder ) > 0 )
+                    <tbody>
+                        @if( count( $importOrder ) > 0 )
 
-                    @foreach( $importOrder as $import )
-                        <li>{{ $import->code }}</li>
-                    @endforeach 
-                @else 
-                    <h2>Chưa có đơn nhập hàng</h2>
-                @endif
+                            @foreach( $importOrder as $import )
+                                <tr>
+                                    <td><a href="/orders/import/{{$import->id}}">{{$import->code}}</a></td>
+                                    <td>
+                                        @if ( $import->location == 1 )
+                                            Kho Nơ Trang Long
+                                        @else 
+                                            Kho Tân Tạo
+                                        @endif
+                                    </td>
+                                    <td>{{ $import->created_at }}</td>
+                                    <td>{{ $import->deadline }}</td>
+                                    <td>
+                                        @if ( $import->status == "wait" ) 
+                                            Chờ duyệt
+                                        @else 
+                                            Đã duyệt
+                                        @endif
+                                    </td>
+                                </tr>
+                            @endforeach 
+                        @else 
+                            <h2>Chưa có đơn nhập hàng</h2>
+                        @endif    
+                    </tbody>
 
-                </ul>
+                </table>
+
+                
                 
             </div>
 
