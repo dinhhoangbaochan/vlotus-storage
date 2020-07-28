20ce6b190232080560359ac5b5cf02aaa5b01431
dinhhoangbaochan <dinhhoangbaochan@gmail.com>
2020-07-28 15:23:49 +0700

    Update all_export.blade.php

--- resources/views/order/export/all_export.blade.php
+++ resources/views/order/export/all_export.blade.php
@@ -19,7 +19,7 @@
 
                 <div class="action_box d-flex align-items-center justify-content-between">
                     <h2 class="main_content__title">Đơn hàng nhập kho</h2>
-                    <a href="{{ url('') }}" class="btn btn-outline-dark">Tạo đơn xuất +</a>
+                    <a data-toggle="modal" data-target="#selectLocation" class="btn btn-outline-dark">Tạo đơn xuất +</a>
                 </div>
 
                 <table class="lotus_table">
@@ -44,5 +44,24 @@
         </div>
 
     </div>
+
+<!-- Modal -->
+<div class="modal fade" id="selectLocation" tabindex="-1" role="dialog" aria-labelledby="selectLocation" aria-hidden="true">
+	<div class="modal-dialog" role="document">
+		<div class="modal-content">
+			<div class="modal-header">
+				<h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+				<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+					<span aria-hidden="true">&times;</span>
+				</button>
+			</div>
+			<div class="modal-body">
+				<a href={{ url('orders/create-export/location_1') }}>Kho Nơ Trang Long</a>
+				<a href="{{ url('orders/create-export/location_2') }}">Kho Tân Tạo</a>
+			</div>
+		</div>
+	</div>
+</div>
+
 
 @endsection
