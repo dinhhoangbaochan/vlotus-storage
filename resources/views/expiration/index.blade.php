29fc415e91b6ce3ac268b0e329647358651e28c2
dinhhoangbaochan <dinhhoangbaochan@gmail.com>
2020-08-01 11:13:57 +0700

    Create index.blade.php

---
+++ resources/views/expiration/index.blade.php
@@ -0,0 +1,30 @@
+@extends('layouts.app')
+
+
+@section('content')
+<div class="row m-0">
+	
+	<div class="col-2 p-0">
+		<div class="left_sidebar">
+			@include('inc.sidebar')
+		</div>
+	</div>
+
+	<div class="col-10 p-0">
+
+		@include('inc.navbar')
+		@include('inc.message')
+
+		<div class="container pt-4">
+			<div class="action_box d-flex align-items-center justify-content-between">
+				<h2 class="main_content__title">Quản lý Hạn Sử Dụng</h2>
+				<a href="/storage/create" class="btn btn-outline-dark">Thêm HSD +</a>
+			</div>
+
+
+		</div>
+	</div>
+
+</div>
+
+@endsection
