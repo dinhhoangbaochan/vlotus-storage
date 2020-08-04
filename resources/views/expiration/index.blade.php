da6af186c26c036fb35a9b80342dcefdbd1d9e44
dinhhoangbaochan <dinhhoangbaochan@gmail.com>
2020-08-04 19:09:44 +0700

    Update index.blade.php

--- resources/views/expiration/index.blade.php
+++ resources/views/expiration/index.blade.php
@@ -21,10 +21,40 @@
 				<a href="/storage/create" class="btn btn-outline-dark">Thêm HSD +</a>
 			</div>
 
-			@foreach ($productsInStorage as $item)
-				@php $currentProducts = $products::find( $item->p_id ) @endphp
-				<li>{{ $currentProducts->name }}</li>
-			@endforeach
+			<table class="lotus_table">
+                    
+				<thead>
+					<tr>
+						<th>Hình ảnh</th>
+						<th>Tên sản phẩm</th>
+						<th>Nơi lưu trữ</th>
+						<th>Số lượng thực tế</th>
+					</tr>
+				</thead>
+
+				@if ( count( $productsInStorage ) > 0 )
+
+					<tbody>
+						
+						@foreach ($productsInStorage as $item)
+							@php $currentProducts = $products::find( $item->p_id ) @endphp
+							<tr>
+								<td>{{ $currentProducts->product_image }}</td>
+								<td>{{ $currentProducts->name }}</td>
+								<td>{{ $item->location }}</td>
+								<td>{{ $item->amount }}</td>
+							</tr>
+						@endforeach
+						
+					</tbody>
+
+				@else
+					
+				@endif
+
+				
+
+			</table>
 
 		</div>
 	</div>
