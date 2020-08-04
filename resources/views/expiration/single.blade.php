96a0f3c6fd6d3e681a63b46d34a65ea72a4e476e
dinhhoangbaochan <dinhhoangbaochan@gmail.com>
2020-08-04 20:37:33 +0700

    Update single.blade.php

--- resources/views/expiration/single.blade.php
+++ resources/views/expiration/single.blade.php
@@ -1,3 +1,12 @@
+{{-- 
+    
+    ID and Location that we have here, is NOT the ID from products table. 
+    It's from the products_in_storage table. 
+
+    - ID is p_id
+    
+--}}
+
 @extends('layouts.app')
 
 
@@ -13,12 +22,17 @@
 	<div class="col-10 p-0">
 
 		@include('inc.navbar')
-		@include('inc.message')
+        @include('inc.message')
+        
+        @php
+            $thisProduct = $products::find($id);
+        @endphp
 
 		<div class="container pt-4">
 			<div class="action_box d-flex align-items-center justify-content-between">
-				<h2 class="main_content__title">Quản lý Hạn Sử Dụng</h2>
+            <h2 class="main_content__title">Tạo Hạn Sử Dụng cho {{ $thisProduct->name }}</h2>
 			</div>
+            
 
 		</div>
 	</div>
