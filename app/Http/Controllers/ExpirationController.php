8d7b0ba930eb197940052d33a0202026ad04d0e8
dinhhoangbaochan <dinhhoangbaochan@gmail.com>
2020-08-04 18:18:56 +0700

    Update ExpirationController.php

--- app/Http/Controllers/ExpirationController.php
+++ app/Http/Controllers/ExpirationController.php
@@ -4,12 +4,14 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\ProductsInStorage;
+use App\Products;
 
 class ExpirationController extends Controller
 {
     public function index() {
 
         $productsInStorage = ProductsInStorage::all();
+        $products = new Products;
 
         $data = array(
             'productsInStorage'     =>      $productsInStorage,
