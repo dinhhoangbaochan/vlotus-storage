bac0cf8c71f10ad1c847b513c16603a3ee1dd4f2
dinhhoangbaochan <dinhhoangbaochan@gmail.com>
2020-08-10 14:25:33 +0700

    Update edit.blade.php

--- resources/views/expiration/edit.blade.php
+++ resources/views/expiration/edit.blade.php
@@ -7,7 +7,7 @@
 	
 	<div class="col-2 p-0">
 		<div class="left_sidebar">
-			@include('inc.sidebar')
+			@include('inc.sidebar') 
 		</div>
 	</div>
 
@@ -27,30 +27,32 @@
                 <h2 class="main_content__title">Tạo Hạn Sử Dụng cho {{ $thisProduct->name }}</h2>
 			</div>
             <form id="expirationForm">
-            <table class="lotus_table">
-                <thead>
-                    <tr>
-                        <th><img src="/uploaded/{{$thisProduct->product_image}}" alt=""></th>
-                        <th>{{ $thisProduct->name }}</th>
-                        <th>Kho: @if ($location == 1) Nơ Trang Long @else Tân Tạo @endif</th>
-                        <th>Tồn kho: {{ $realProduct->amount }}</th>
-                    </tr>
-                </thead>
-                
-                
-                    <tbody id="expirationDates">
-                        @foreach($getDate as $amount => $date)
+                <table class="lotus_table">
+                    <thead>
                         <tr>
-                            <td>Phiên bản</td>
-                            <td>Số lượng: <input type='number' class='form-control' name='expAmount' value="{{$amount}}" /></td>
-                        <td>Ngày hết hạn: <input type='date' class='form-control' name='expDate' value="{{ $date }}" /></td>
-                            <td></td>
+                            <th><img src="/uploaded/{{$thisProduct->product_image}}" alt=""></th>
+                            <th>{{ $thisProduct->name }}</th>
+                            <th>Kho: @if ($location == 1) Nơ Trang Long @else Tân Tạo @endif</th>
+                            <th>Tồn kho: {{ $realProduct->amount }}</th>
                         </tr>
-                        @endforeach
-                        
-                    </tbody>
-                
-            </table>
+                    </thead>
+                    
+                    
+                        <tbody id="expirationDates">
+                            @foreach($getDate as $id => $expArr)
+                                @foreach ($expArr as $amount => $date)
+                                    <tr>
+                                        <td>Phiên bản</td>
+                                        <td>Số lượng: <input type='number' class='form-control' name='expAmount' value="{{$amount}}" /></td>
+                                        <td>Ngày hết hạn: <input type='date' class='form-control' name='expDate' value="{{ $date }}" /></td>
+                                    </tr>   
+                                @endforeach
+                            
+                            @endforeach
+                            
+                        </tbody>
+                    
+                </table>
             </form>
 		</div>
 	</div>
