9647cbe200239e594bb1915f7142a0722d63b42d
dinhhoangbaochan <dinhhoangbaochan@gmail.com>
2020-08-13 11:11:17 +0700

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/get-api', function() {
+	return ['message'	=>	'hey brother'];
+});
+
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
