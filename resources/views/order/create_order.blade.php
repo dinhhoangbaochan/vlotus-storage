8af71698c032fc70bb954358048aa01e8d287b68
dinhhoangbaochan <dinhhoangbaochan@gmail.com>
2020-08-20 14:04:12 +0700

    Update create_order.blade.php

--- resources/views/order/create_order.blade.php
+++ resources/views/order/create_order.blade.php
@@ -24,7 +24,7 @@
                             <form id="formOne">
                                 <div class="product_info">
                                     <div class="dropdown">
-                                        <input type="text" class="w-100" id="look_for_product" name="findProducts" value="" placeholder="Nhập tên sản phẩm cần thêm vào đơn hàng">
+                                        <input type="text" class="w-100 search-input" id="look_for_product" name="findProducts" value="" placeholder="Nhập tên sản phẩm cần thêm vào đơn hàng">
                                         <div id="findProductList" class="dropdown-menu" aria-labelledby="">
                                         </div>
                                     </div>   
